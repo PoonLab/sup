f125462ca67e3043b4abdfda0beaef3a86730dd6
davidchampredon <david.champredon@gmail.com>
2019-10-24 11:11:39 -0400

    scripts to simulate

---
+++ src/go.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+./clean.sh
+
+Rscript simul-seq.R
+# Rscript sequnc.R
+# ./calc-tree.sh
+# Rscript ss.R
+
---
+++ src/simul-seq.R
@@ -0,0 +1,66 @@
+## 
+##  Simulate evolution of a 
+##  "root" nucleotide sequence 
+##
+
+library(phylosim, quietly = TRUE, warn.conflicts = F)
+library(dplyr,quietly = TRUE, warn.conflicts = F)
+
+set.seed(1234)
+
+message("\nStarting phylogeny simulation...")
+
+# Define the evolution process:
+ev.proc    <- phylosim::JC69()  
+summary(ev.proc)
+
+# Define the "root" sequence from
+# which evolution occurs:
+root.seq.length <- 60
+root.seq <- NucleotideSequence(length = root.seq.length, 
+                               processes = list(list(ev.proc)) ) %>%
+    sampleStates()
+print("The root sequence is:")
+print(root.seq)
+print(paste("Root sequence length:", root.seq.length))
+
+# Draw invariable positions 
+invar.n   <- 5
+invar.pos <- sample(1:root.seq.length, invar.n)
+print("Invariant positions:")
+print(sort(invar.pos))
+setRateMultipliers(root.seq,ev.proc,0,invar.pos)
+# getRateMultipliers(root.seq,ev.proc)
+
+
+# Define the evolution tree
+# and how spreaded it is:
+n.tips   <- 10
+tree.sim <- rcoal(n.tips)
+message(paste('Tree with',n.tips,'tips built.'))
+
+# Simulate evolution of 
+# the root seq on the tree:
+sim <- PhyloSim(phy  = tree.sim,
+                root = root.seq )
+Simulate(sim)
+
+# Save
+fname <- 'seqs/sim.fasta'
+phylosim::saveAlignment(this = sim, 
+                        file = fname, 
+                        skip.internal = TRUE, 
+                        paranoid = TRUE)
+# print(sim$alignment)
+
+message(paste("Simulated phylogeny saved in:",
+              fname))
+
+pdf('plot-sim-phylo.pdf', 
+    width=15, height = 10)
+plot(ev.proc)
+plot(tree.sim, type = 'clado')
+plot(sim, num.pages = 1)
+dev.off()
+
+message("Phylogeny simulation done.")
