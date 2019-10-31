e4e65f5e343c459aee9675d2ff66efeab2a876f4
davidchampredon <david.champredon@gmail.com>
2019-10-31 12:37:23 -0400

    add entropy to plots

--- data/zanini/tmp.R
+++ data/zanini/tmp.R
@@ -45,22 +45,6 @@ read_data <- function(patient, timepoint) {
 
 
 
-
-digest <- function(dat) {
-    df <- dat %>%
-        mutate(total = A+C+T+G+d+N)
-    
-    dfp <- (df / df$total) %>%
-        select(-total) %>%
-        mutate(position=1:nrow(df))
-    
-    dfpb <- dfp %>%
-        gather(key='base', value='p', -position) 
-    
-    return(list(df=df, dfp=dfp, dfpb=dfpb))
-}
-
-
 calc_entropy_one <- function(p){
     res <- NA
     if(p==0) res <- 0
@@ -68,28 +52,52 @@ calc_entropy_one <- function(p){
     return(res)
 }
 
-calc_entropy <- function(dat) {
-    z <- digest(dat)
-    dfpb = z$dfpb
+
+calc_entropy <- function(dfp) {
+    # Calculate "p*log(p)" for 
+    # each position and base:
+    tmp <- list()
+    for(b in c('A','C','G','T','d','N')){
+        tmp[[b]] <- sapply(dfp[[b]], calc_entropy_one)
+    }
     
-    a <- dfpb %>%
-        group_by(position) %>%
-        summarise(entropy = calc_entropy_one(p))
-    
-    # STOPPED HERE
-    # GTG 
+    # Entropy for each position:
+    m <- do.call('cbind', tmp)
+    dfp$entropy <- apply(m, MARGIN = 1, FUN=sum)    
+    return(dfp)
 }
 
 
+digest <- function(dat) {
+    
+    df <- dat %>%
+        mutate(total = rowSums(.[1:ncol(dat)]))
+    
+    dfp <- (df / df$total) %>%
+        select(-total) %>%
+        mutate(position = 1:nrow(df))
+    
+    dfpb <- dfp %>%
+        gather(key='base', value='p', -position) 
+    
+    # Add after dfpb creation to avoid messing up (?)
+    dfp <- calc_entropy(dfp)
+    
+    return(list(df=df, dfp=dfp, dfpb=dfpb))
+}
+
 plots <- function(dat) {
-    z <- digest(dat)
-    df = z$df
-    dfpb = z$dfpb
+    z    <- digest(dat)
+    df   <- z$df
+    dfp  <- z$dfp
+    dfpb <- z$dfpb
+    
     fname2 <- attr(dat, 'fname2')
     
     # Coverage:
-    g.cvg <- df %>%
-        mutate(position = 1:nrow(df)) %>%
+    dfc <- df %>%
+        mutate(position = 1:nrow(df))
+    g.cvg <- dfc %>%
         ggplot(aes(x=position, y=total)) + 
         geom_line() +
         scale_y_log10()+
@@ -104,7 +112,7 @@ plots <- function(dat) {
                  stat = 'identity')+
         ggtitle(fname2)
     
-    
+    # Histogram of probabilities by base:
     g.histp <- dfpb %>%
         ggplot(aes(x=p)) +
         geom_histogram(aes(fill=base), bins=10)+
@@ -112,8 +120,58 @@ plots <- function(dat) {
         scale_y_log10()+
         ggtitle(fname2)
     
+    # Histogram of entropy by position:
+    g.histe <- dfp %>%
+        ggplot(aes(x=entropy))+
+        geom_histogram(binwidth = 0.05)+
+        scale_y_log10()+
+        ggtitle(fname2)
+    g.histe
+    
+    # Entropy by position
+    bucket <- 50
+    g.pose <- dfp %>%
+        mutate(roundpos = round(position/bucket)*bucket) %>%
+        group_by(roundpos) %>%
+        summarise(m.entropy = mean(entropy),
+                  min.entropy = min(entropy),
+                  max.entropy = max(entropy)) %>%
+        ggplot(aes(x=roundpos)) + 
+        geom_point(aes(y=m.entropy), size=0.3) + 
+        geom_line(aes(y=m.entropy), size=0.3) + 
+        geom_ribbon(aes(ymin=min.entropy, 
+                        ymax=max.entropy),
+                        alpha=0.4)+
+        ggtitle(fname2, 
+                paste('Bucket size =',bucket, 
+                      '; Total entropy =', round(sum(dfp$entropy),3)))
+    # g.pose
+    
+    # Entropy & coverage
+    
+    g.pose2 <- g.pose + 
+        geom_line(data = dfc, 
+                  aes(x=position, y=log(total)/max(log(total))),
+                  colour = 'blue', alpha=0.8)
+    
+    dfj <- data.frame(entropy=dfp$entropy, 
+                      cvg = dfc$total,
+                      position = dfc$position)
+    
+    g.cvgent <- dfj %>% 
+        ggplot()+
+        geom_point(aes(y=entropy, 
+                       x=cvg,
+                       colour = position),
+                   alpha = 0.25, size=3)+
+        scale_x_log10()+
+        scale_y_log10()
+    
+    
     fplot <- paste0('plot-',fname2,'.pdf')
     pdf(file = fplot, width=12, height = 10)
+    plot(g.pose2)
+    plot(g.cvgent)
     plot(g.histp)
     plot(g.cvg)
     # plot(g.pviz)
@@ -123,7 +181,7 @@ plots <- function(dat) {
 # --- RUN ----
 
 patient.vec   <- c(1,3,9,11)
-timepoint.vec <- c(1,5,0)
+timepoint.vec <- c(1,3,5,0)
 
 for(patient in patient.vec){
     for(timepoint in timepoint.vec){
