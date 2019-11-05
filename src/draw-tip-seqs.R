87842ff56b8d7eb9ded75ca915d2955ca96e0ceb
davidchampredon <david.champredon@gmail.com>
2019-11-05 16:22:01 -0500

    error probability stocahstic - GTG

--- src/draw-tip-seqs.R
+++ src/draw-tip-seqs.R
@@ -13,26 +13,37 @@ source('utils.R')
 set.seed(1234)
 prm <- read.csv('prm.csv')
 
-# Error probability of base call.
-# Fixed and the same across 
-# all sequences and positions
-err.prob <- get_prm(prm, 'err.proba')
-p <- 1 - err.prob
-
 # Read the simulated phylogeny:
 M.list <- list()
 a <- read.fasta(file = 'seqs/sim.fasta', 
                 forceDNAtolower = FALSE)
 
+# Error probability of base call.
+# Fixed and the same across 
+# all sequences and positions
+err.prob <- get_prm(prm, 'err.proba')
+s1 = 0.1
+s2 = s1/err.prob-s1
+# s1 = s2 / (1-err.prob)
+n = get_prm(prm, 'phylosim.root.seq.length')
+err.prob <- rbeta(n=n, shape1 = s1, shape2 = s2)
+hist(err.prob)
+p <- 1 - err.prob
+
+
 # Add uncertainty by transforming the 
 # (certain) sequence string into a 
 # probabilistic sequence:
-for(i in 1:length(a)){
+seq.entropy <- list()
+for(i in 1:length(a)){  #i=1
     seq <- a[[i]]
     seq <- seq[seq!=' '] 
-    M.list[[i]] <- build_from_seq(seq, p)
+    M.list[[i]]<- build_from_seq(seq, p)
+    seq.entropy[[i]] <- apply(M.list[[i]] , MARGIN=2, FUN=entropy)
 }
-
+sum.entropy <- sapply(seq.entropy, sum, na.rm=T)
+sum.entropy
+plot(seq.entropy[[1]])
 
 # Number of times we sample
 # from the probabilistic sequences
