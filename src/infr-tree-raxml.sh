3533c3932233c5b467412d32fda7e38ca9be5043
davidchampredon <david.champredon@gmail.com>
2019-11-19 09:08:43 -0500

    unroot RAxML outputs

--- src/infr-tree-raxml.sh
+++ src/infr-tree-raxml.sh
@@ -4,7 +4,7 @@
 # sampled probabilistic sequences
 
 # RAxML documentation:
-# http://www.microbesonline.org/fasttree/
+# https://cme.h-its.org/exelixis/web/software/raxml/index.html
 
 # How many Monte Carlo samples:
 N=$(ls -l seqs/seqs-mc*.fasta | wc -l)
@@ -19,10 +19,21 @@ TREE_NAME="tree-raxml-mc"
 
 for i in $(seq 1 $N) 
 do
- raxmlHPC $RAXML_OPT -s seqs/seqs-mc-$i.fasta -n $TREE_NAME-$i.out
+  # Phylogeny inference:
+  raxmlHPC $RAXML_OPT -s seqs/seqs-mc-$i.fasta -n $TREE_NAME-$i.out
 done
 
-mv RAxML_*$TREE_NAME*.out trees/
+echo "-------- Unrooting ..."
+# Remove the `:0.0` string that 
+# symbolizes rooting in RAxML tree:
+for i in $(seq 1 $N) 
+do
+  sed -i -e 's/:0.0;/;/g' RAxML_bestTree.$TREE_NAME-$i.out
+done
+
+# RAxML can only output in the local directory,
+# so move outfiles manually:
+mv RAxML_*$TREE_NAME*.out* trees/
 
 echo "Reconstruction of all MC trees with RAxML done."
 echo "RAxML outputs saved in trees/tree-mc-raxmli.out."
