814d4179a196ab118da71bcf3b7f104d9248325a
davidchampredon <david.champredon@gmail.com>
2019-11-19 11:00:40 -0500

    minor edits

--- src/clean.sh
+++ src/clean.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 rm -rf seqs/seqs-*.fasta
-rm -rf trees/tree-*-mc*.nwk
-rm -rf trees/tree-*-mc*.out
+rm -rf trees/tree*mc*.out*
+rm -rf trees/tree*mc*.nwk*
+rm -rf trees/RAxML*.out*
 
 rm -rf plot*.pdf
 rm -rf *.RData
