b3d00010c0046948d93e0e611a41b8bb77c43199
davidchampredon <david.champredon@gmail.com>
2019-11-21 17:10:11 -0500

    Update invariant-prop.R

--- data/zanini/invariant-prop.R
+++ data/zanini/invariant-prop.R
@@ -5,51 +5,62 @@ library(ggplot2) ; theme_set(theme_bw())
 source('utils-zanini.R')
 
 
-# ---- Data ----
+# ---- Data
 
-pat <- 1
-tpt <- c(1:5,0)
-nt <- length(tpt)
-dfl <- list()
-
-for (i in seq_along(tpt)) {
-    dfl[[i]] <- read_data(patient = pat, 
-                          timepoint = tpt[i])
-    dfl[[i]]$tpt <- tpt[i]
-    nr <- nrow(dfl[[i]])
-    dfl[[i]]$pos <- 1:nr
+#' Estimates the proportion of invariant position bases
+#' for one patient from her/his longitudinal sequences (Zanini's data)
+#' @param pat Integer. Patient number
+#' @param tpt Integer vector. Time points of the sampled sequences to include.
+#' @param thresh Numeric. Threshold frequency of a nucleotide to consider a position invariant.
+estim_prop_invariant <- function(pat, tpt, thresh) { #pat=2
+    
+    print(paste('Patient',pat,'...'))
+    
+    nt <- length(tpt)
+    dfl <- list()
+    
+    for (i in seq_along(tpt)) {
+        dfl[[i]] <- read_data(patient = pat, 
+                              timepoint = tpt[i])
+        dfl[[i]]$tpt <- tpt[i]
+        nr <- nrow(dfl[[i]])
+        dfl[[i]]$pos <- 1:nr
+    }
+    
+    # Convert to probabilities:
+    df <- do.call('rbind', dfl) %>%
+        select(-d, -N) %>%
+        mutate(n = A+C+G+T)
+    df[,1:4] <- df[,1:4] / df$n
+    
+    df <- select(df, -n)
+    
+    # ---- Estimation
+    
+    df2 <- df %>%
+        pivot_longer(-c('tpt','pos'),
+                     names_to = 'base',
+                     values_to = 'proba') %>%
+        mutate(test1 = proba > thresh)
+    
+    dfs <- df2 %>% 
+        group_by(pos, base) %>%
+        summarize(a = sum(test1)) %>%
+        mutate(base.is.invar = (a==nt))
+    
+    df.inv <- dfs %>%
+        group_by(pos) %>%
+        summarize(pos.invar = max(base.is.invar))
+    
+    res <- mean(df.inv$pos.invar, na.rm = TRUE)
+    return(res)
 }
 
-
-# Convert to probabilities:
-df <- do.call('rbind', dfl) %>%
-    select(-d, -N) %>%
-    mutate(n = A+C+G+T)
-df[,1:4] <- df[,1:4] / df$n
+pat.vec <- c(1:11)
+tpt <- c(1:2,0)
+thresh <- 0.99
 
 
-# ---- Estimation ----
+x <- sapply(pat.vec, estim_prop_invariant, tpt=tpt,thresh=thresh)
 
-
-# Select only the positions that have
-# a probability above the threshold:
-thresh <- 0.999
-dft <- df %>%
-    mutate(mono.a = A>thresh,
-           mono.c = C>thresh,
-           mono.g = G>thresh,
-           mono.t = T>thresh)
-
-
-# variance of presence across time points:
-dfv <- dft %>%
-    group_by(pos) %>%
-    summarise(cv.a = sd(A)/mean(A),
-              cv.c = sd(C)/mean(C),
-              cv.g = sd(G)/mean(G),
-              cv.t = sd(T)/mean(T),
-              n=n()) %>%
-    mutate()
-dfv
-    
-mean(dfs$is.invar)
+hist(x)
