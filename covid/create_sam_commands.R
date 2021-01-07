c8e530e579926d1ed7cb67586b6952e8c20ebb49
DBecker7 <d7becker@gmail.com>
2021-01-06 22:28:57 -0500

    Initial changes

---
+++ covid/create_sam_commands.R
@@ -0,0 +1,54 @@
+# Copy and paste (or drag and drop) links here.
+# Delete when done
+
+urls1 <- "https://www.ncbi.nlm.nih.gov/sra/SRX9537817[accn]
+https://www.ncbi.nlm.nih.gov/sra/SRX9537816[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4615584[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4615465[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4614837[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4614688[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4614620[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4614165[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4614147[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4614119[accn]
+https://www.ncbi.nlm.nih.gov/sra/SRX9233401[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4613447[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4590348[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4582858[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4307528[accn]
+https://www.ncbi.nlm.nih.gov/sra/ERX4307447[accn]
+"
+
+
+urls <- strsplit(urls1, split = "\n")[[1]]
+
+commands <- c()
+for(i in seq_along(urls)[-(1:7)]){
+    test1 <- xml2::read_html(urls[i])
+    test2 <- as.character(test1)
+    pos <- gregexpr("run=", test2)[[1]]
+    print(substr(test2, pos + 3, pos + 15))
+    asc <- substr(test2, pos + 4, pos + 14)
+    com <- paste0("sam-dump ", asc, " > ", asc, ".sam")
+    commands <- c(commands, com)
+}
+cat(commands, sep = "\n")
+
+" Copying and pasting the output here to modify codes and also not barrage the server
+sam-dump SRR13092001 > SRR13092001.sam # timed out
+sam-dump SRR13092002 > SRR13092002.sam
+sam-dump ERR4694498 > ERR4694498.sam
+sam-dump ERR4694380 > ERR4694380.sam
+sam-dump ERR4758772 > ERR4758772.sam
+sam-dump ERR4693605 > ERR4693605.sam
+sam-dump ERR4693537 > ERR4693537.sam
+sam-dump ERR4693079 > ERR4693079.sam
+sam-dump ERR4693061 > ERR4693061.sam
+sam-dump ERR4693034 > ERR4693034.sam
+sam-dump SRR12762573 > SRR12762573.sam
+sam-dump ERR4692364 > ERR4692364.sam
+sam-dump ERR4667618 > ERR4667618.sam
+sam-dump ERR4664555 > ERR4664555.sam
+sam-dump ERR4363387 > ERR4363387.sam
+sam-dump ERR4364007 > ERR4364007.sam
+"
---
+++ covid/open_seSAMe_single.R
@@ -0,0 +1,31 @@
+# To run in background (esp. on rei): 
+# nohup Rscript open_seSAMe2.R 
+
+t0 <- Sys.time()
+setwd("/home/dbecker7/run_sam")
+
+# Using parse.sam
+
+source("parse-sam.r")
+
+est.length <- length(count.fields("SRR13020989.sam"))
+
+test1 <- parse.sam("SRR13020989.sam", est.length = est.length)
+#test2 <- parse.sam.mp("~/PostDoc Fasta Files/sra-downloads/files/SRR13020989_2.sam",
+#    n.cores = 6)
+# The parallel version (parse.sam.mp) recalculates phred at each position. Yes,
+# it's parallel, but with a literal metric ton of calculation overhead.
+#str(test1)
+
+saveRDS(test1, file = "open_seSAMe2-S.RDS")
+
+tt <- round(difftime(Sys.time(), t0, units = "hours"), 3)
+msg1 <- paste0("Completed in ", tt, " hours.")
+msg2 <- paste0("Result has ", ncol(test1), " cols and ", nrow(test1), " rows.")
+msg <- paste(msg1, msg2, collapse = "/n")
+print(msg)
+writeLines(msg, "open_seSAM2_info.txt")
+
+
+
+
