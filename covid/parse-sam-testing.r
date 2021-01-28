37dad4df6f183242ffc2d4dd111d052cb4940a1d
DBecker7 <d7becker@gmail.com>
2021-01-28 12:19:47 -0500

    Minor speedups and verbosity changes, remove some code for paired reads.

--- covid/parse-sam-testing.r
+++ covid/parse-sam-testing.r
@@ -1,6 +1,5 @@
 source("covid/parse-sam.r")
 
-
 t0 <- Sys.time()
 test1 <- parse.sam("covid/SRR13020990_small.sam")
 parsetime <- difftime(Sys.time(), t0, units = "mins")
@@ -10,3 +9,18 @@ timeperline <- parsetime/500 # this file has 500 lines
 
 # Total time for the file (150,000 lines), in hours
 paste(round(as.numeric(timeperline)*147654/60, 3), "hours")
+
+source("covid/parse-sam_dev.r")
+tn <- Sys.time()
+test2 <- parse.sam_dev("covid/SRR13020990_small.sam")
+parsetime <- difftime(Sys.time(), tn, units = "mins")
+parsetime
+
+timeperline <- parsetime/500 # this file has 500 lines
+
+# Total time for the file (150,000 lines), in hours
+paste(round(as.numeric(timeperline)*147654/60, 3), "hours")
+
+all.equal(test1, test2)
+
+
