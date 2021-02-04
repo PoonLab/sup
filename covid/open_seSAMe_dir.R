25acb7d470305033086aa12dceb9ebf5c37637b3
DBecker7 <d7becker@gmail.com>
2021-02-04 11:28:20 -0500

    Minor updates to usability

--- covid/open_seSAMe_dir.R
+++ covid/open_seSAMe_dir.R
@@ -1,20 +1,15 @@
 # To run in background (esp. on rei): 
-# nohup Rscript open_seSAMe_multi.R ERR4305816.sam ERR4085809.sam ERR4204823.sam ERR4307842.sam
+# nohup Rscript open_seSAMe_dir [dirname] &
 
 source("parse-sam.r")
 
+args <- commandArgs(TRUE)
+
 # This file assumes a particular directory structure.
 # SAM files must either be in current_dir or current_dir/samTODO
-samfile <- list.files(path = "samTODO", pattern = "*.sam")
+samfile <- list.files(path = args[1], pattern = "*\\.sam")
 if(length(samfile) == 0){
-    stop("no files specifed")
-}
-if(!all(grepl(".sam", samfile))){
-    stop("at least one bad file name")
-}
-# Checks current_dir AND samTODO
-if(!all(samfile %in% c(list.files(), list.files("samTODO")))){
-    stop("files must be in directory called samTODO")
+    stop("no files in specified directoy")
 }
 
 if(!dir.exists("samDONE")) dir.create("samDONE")
@@ -22,21 +17,20 @@ if(!dir.exists("samDONE")) dir.create("samDONE")
 print(length(samfile))
 
 for(f in seq_along(samfile)){
+    cat("\n")
     t0 <- Sys.time()
-    thisfilename <- samfile[f]
+    thisfilename <- paste0(args[1], "/", samfile[f])
     print(thisfilename)
     
-    thisacs <- substr(thisfilename, 1, nchar(thisfilename) - 4)
+    thisacs <- substr(samfile[f], 1, nchar(samfile[f]) - 4)
+    thisRDS <- paste0("samDONE/S-", thisacs, ".RDS")
     
-    # Check whether it's current dir or sam TODO
-    if(!thisfilename %in% list.files()){
-        thisfilename <- paste0("samTODO/", thisfilename, collapse = "")
-    }
+    if(file.exists(thisRDS)) next
     
     try({ # don't break loop if there's an error
         test1 <- parse.sam(thisfilename)
         
-        saveRDS(test1, file = paste0("samDONE/open_seSAMe-S-", thisacs, ".RDS"))
+        saveRDS(test1, file = thisRDS)
         
         tt <- round(difftime(Sys.time(), t0, units = "hours"), 3)
         msg1 <- paste0(thisacs, " completed in ", tt, " hours.")
