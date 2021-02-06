688cd779a54472179581368011366e0c60fd7711
DBecker7 <d7becker@gmail.com>
2021-02-05 19:45:35 -0500

    Minor updates

--- covid/parse-sam-testing.r
+++ covid/parse-sam-testing.r
@@ -1,7 +1,7 @@
 source("covid/parse-sam.r")
 
 t0 <- Sys.time()
-test1 <- parse.sam("covid/SRR13020990_small.sam")
+test1 <- parse.sam("data/SRR13592146_small.sam")
 parsetime <- difftime(Sys.time(), t0, units = "mins")
 parsetime
 
@@ -12,7 +12,7 @@ paste(round(as.numeric(timeperline)*147654/60, 3), "hours")
 
 source("covid/parse-sam_dev.r")
 tn <- Sys.time()
-test2 <- parse.sam_dev("covid/SRR13020990_small.sam")
+test2 <- parse.sam_deprecated("covid/SRR13020990_small.sam")
 parsetime <- difftime(Sys.time(), tn, units = "mins")
 parsetime
 
