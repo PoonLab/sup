883848ca7994a4d8a2f311a9c166661ec9160343
DBecker7 <d7becker@gmail.com>
2021-02-11 17:14:54 -0500

    New testing files

--- covid/parse-sam-testing.r
+++ covid/parse-sam-testing.r
@@ -1,7 +1,7 @@
 source("covid/parse-sam.r")
 
 t0 <- Sys.time()
-test1 <- parse.sam("data/SRR13592146_small.sam")
+test1 <- parse.sam("data/SRR13020990_small.sam")
 parsetime <- difftime(Sys.time(), t0, units = "mins")
 parsetime
 
@@ -12,7 +12,7 @@ paste(round(as.numeric(timeperline)*147654/60, 3), "hours")
 
 source("covid/parse-sam_dev.r")
 tn <- Sys.time()
-test2 <- parse.sam_deprecated("covid/SRR13020990_small.sam")
+test2 <- parse.sam_deprecated("data/SRR13020990_small.sam")
 parsetime <- difftime(Sys.time(), tn, units = "mins")
 parsetime
 
@@ -22,6 +22,14 @@ timeperline <- parsetime/500 # this file has 500 lines
 paste(round(as.numeric(timeperline)*147654/60, 3), "hours")
 
 all.equal(test1, test2)
+
+tn <- Sys.time()
+test2 <- parse.sam.dt("data/SRR13020990_small.sam")
+parsetime <- difftime(Sys.time(), tn, units = "mins")
+parsetime
+
+timeperline <- parsetime/500 # this file has 500 lines
+
 
 
 
