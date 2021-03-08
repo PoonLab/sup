9144aead5e402adb2a41436eb0fdc1e478ebaf8b
DBecker7 <d7becker@gmail.com>
2021-03-08 09:41:34 -0500

    Easier to test different function versions

--- covid/parse-sam-testing.r
+++ covid/parse-sam-testing.r
@@ -1,7 +1,11 @@
-source("covid/parse-sam.r")
+source("covid/parse-sam-con.r")
+
+inFile <- "data/SRR13020990_small.sam" # unpaired
+inFile <- "data/ERR5069871.sam" # Paired
+inFile <- "/media/devan/Seagate Basic/PostDoc/files/unpaired/blocku1/ERR4085809.sam"
 
 t0 <- Sys.time()
-test1 <- parse.sam("data/SRR13020990_small.sam")
+test1 <- parse.sam(inFile)
 parsetime <- difftime(Sys.time(), t0, units = "mins")
 parsetime
 
@@ -12,7 +16,7 @@ paste(round(as.numeric(timeperline)*147654/60, 3), "hours")
 
 source("covid/parse-sam_dev.r")
 tn <- Sys.time()
-test2 <- parse.sam_deprecated("data/SRR13020990_small.sam")
+test2 <- parse.sam_deprecated(inFile)
 parsetime <- difftime(Sys.time(), tn, units = "mins")
 parsetime
 
@@ -24,7 +28,7 @@ paste(round(as.numeric(timeperline)*147654/60, 3), "hours")
 all.equal(test1, test2)
 
 tn <- Sys.time()
-test2 <- parse.sam.dt("data/SRR13020990_small.sam")
+test2 <- parse.sam.dt(inFile)
 parsetime <- difftime(Sys.time(), tn, units = "mins")
 parsetime
 
@@ -35,5 +39,5 @@ timeperline <- parsetime/500 # this file has 500 lines
 
 
 # Paired read testing
-test3 <- parse.sam("data/SRR13592146_small.sam")
+test3 <- parse.sam(inFile)
 
