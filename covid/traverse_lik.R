b08ad674693adf7179813441771b839d67483701
DBecker7 <d7becker@gmail.com>
2021-04-07 21:26:06 -0400

    Prototype script finished

--- covid/traverse_lik.R
+++ covid/traverse_lik.R
@@ -1,8 +1,10 @@
 library(dplyr)
 library(tidyr)
+library(here)
 
-sam1 <- readRDS("data/unc_covid/open_seSAMe-S-ERR4363387.RDS")
-
+in_file <- "open_seSAMe-S-ERR4363387.RDS"
+in_path <- "data/unc_covid/"
+sam1 <- readRDS(here(in_path, in_file))
 
 dim(sam1)
 
@@ -42,5 +44,81 @@ non0 <- sam2[to_sample, ] %>%
     mutate(order = order(value))
 
 # Next steps:
-    # 1. Fix everything
-    # 2, Figure it out from there.
+    # 1. Expand grid to include labels of permutations
+    # 2. Use this grid to lookup M and m
+    # 3. Multiply!
+
+bottom_n <- function(S, n) {
+    M_all <- apply(S, 1, max)
+    bottom <- which(rank(M_all) <= n)
+
+    M <- M_all[bottom]
+    m <- apply(S[bottom, ], 1, function(x) {
+        sort(x, decreasing = TRUE)[2]
+    })
+
+    dummy_list <- list()
+    for (i in seq_len(n)) {
+        dummy_list[[i]] <- c(1, 2)
+    }
+
+    subs <- expand.grid(dummy_list)
+    colnames(subs) <- bottom
+    subs$diff_lik <- NA
+
+    for (i in seq_len(nrow(subs))) {
+        new_subs <- which(subs[i, 1:n] == 2)
+        new_lik <- M
+        new_lik[new_subs] <- m[new_subs]
+        subs$diff_lik[i] <- sum(log(new_lik))
+    }
+    subs
+}
+
+# Testing
+lik_mat <- bottom_n(sam2, 12)
+lik_mat <- lik_mat[order(-lik_mat$diff_lik), ]
+head(tmp)
+nrow(tmp)
+
+# now:
+    # Calculate conseq
+    # make the top 1000 substitutions
+    # Run through pangolin
+alph <- colnames(sam1)
+conseq <- apply(S, 1, function(x) {
+    if (any(is.na(x)) | sum(x) < 1) {
+        return("N")
+    } else {
+        return(alph[which.max(x)])
+    }
+})
+
+runner_up <- apply(S, 1, function(x) {
+    if (any(is.na(x)) | sum(x) < 1) {
+        return("N")
+    } else {
+        return(alph[order(-x)[2]])
+    }
+})
+
+ordered_seq <- lapply(1:1000, function(x) conseq)
+for (i in seq_along(ordered_seq)) {
+    switch <- lik_mat[i, 1:(ncol(lik_mat) - 1)]
+    to_switch <- colnames(switch)[as.numeric(switch) == 2]
+    to_switch <- as.numeric(to_switch)
+    ordered_seq[[i]][to_switch] <- runner_up[to_switch]
+}
+
+conseq <- paste(conseq, collapse = "")
+ordered_seq <- sapply(ordered_seq, paste, collapse = "")
+
+acc <- rev(strsplit(strsplit(in_file, "\\.")[[1]][1], "-")[[1]])[1]
+fasta_labels <- paste("> ", acc,
+        c(0, round(lik_mat$diff_lik, 6)),
+    sep = "")
+
+to_save <- paste(fasta_labels, c(conseq, ordered_seq),
+    collapse = "\n", sep = "\n")
+
+writeLines(to_save, here("data/ord_covid", "test1.fasta"))
