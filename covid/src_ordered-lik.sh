212149b24ef2a99861174e36e9719a43f96e1f84
DBecker7 <d7becker@gmail.com>
2021-04-13 15:36:24 -0400

    Include ags and rmd

--- covid/src_ordered-lik.sh
+++ covid/src_ordered-lik.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
+
+# I'm bad at arg parsing
+N=5
+overwrite=false
+num_args=$#
+if [ $num_args -gt 0 ]; then
+    for (( i=1; i <= $#; i++)); do
+        arg=${!i}
+        if [ $arg = -N ]; then
+            arg2_pos=$((i+1))
+            N=${!arg2_pos}
+        fi
+
+        if [ $arg = --overwrite ]; then
+            overwrite=true
+        fi
+    done
+fi
+
+
+
 # Create files
-Rscript covid/ord_S.R -N 1000
+if [ $overwrite = true ]; then
+    Rscript covid/ord_S.R -N $N --overwrite
+else 
+    Rscript covid/ord_S.R -N $N
+fi
+
 
 
 source ~/anaconda3/etc/profile.d/conda.sh
@@ -19,3 +45,6 @@ do
     out_fasta="data/pangordlineages/"$newacc2"_pangolineages.csv"
     pangolin $sampled_fasta --outfile $out_fasta
 done
+
+
+Rscript -e "rmarkdown::render('figures/ord-results.Rmd')"
