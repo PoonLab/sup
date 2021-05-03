a74051696969d77a4919e67bff15fe3f27958bd4
DBecker7 <d7becker@gmail.com>
2021-05-03 16:36:01 -0400

    Minor updates

--- src/run-multi.sh
+++ src/run-multi.sh
@@ -2,6 +2,8 @@
 
 ./clean.sh
 
+# Install missing R packages where Rscript can find them.
+Rscript check-R-packages.R
 
 # Generate the true phylogeny.
 # Sequences saved in "seqs/sim.fasta"
@@ -14,12 +16,12 @@ RAXML_OPT="-m GTRGAMMA -p 12345 --JC69 --silent" # <-- make sure it's the same a
 raxmlHPC $RAXML_OPT -s seqs/sim.fasta -n tree-raxml-certain.out > trees/certain.out
 mv RAxML_*certain*.out* trees/
 
-# Tree inference for 
+# Tree inference for
 # probabilistic sequences:
 N_PRM_SET=$(wc -l < prm-btshp.csv)
 
 pids=""
-for i in $(seq 1 $N_PRM_SET)  
+for i in $(seq 1 $N_PRM_SET)
 do
   # This is run in parallel:
   ./run-unit.sh $i &
