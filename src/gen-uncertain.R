b37629b45af44f7bfba0d4e161d8dc319246f38e
DBecker7 <d7becker@gmail.com>
2021-05-04 17:02:12 -0400

    Change prm_del to work with length of 300, tame linr.

--- src/gen-uncertain.R
+++ src/gen-uncertain.R
@@ -7,54 +7,53 @@ message("gen-uncertain.R")
 
 system("figgy=`whereis figlet`; figgylen=${#figgy}; if [ $figgylen > 10 ]; then figlet gen-uncertain.R; fi")
 
-source('utils.R')
+source("utils.R")
 
 # Read which parameter set to use:
 args <- commandArgs(trailingOnly = TRUE) # args = 4 # DEBUG
 
 # Observed sequence:
-fastafile <- 'seqs/sim.fasta'
+fastafile <- "seqs/sim.fasta"
 
 # Parameter shape for the Beta-Uniform uncertainty model:
-btshp <- load_beta_shapes(fname.prm = 'prm-btshp.csv', args = args)
+btshp <- load_beta_shapes(fname.prm = "prm-btshp.csv", args = args)
 
 
 # Load other parameters:
-prm <- read.csv('prm.csv')
-n.repl <- get_prm(prm,'sample.tips.n.mc')
+prm <- read.csv("prm.csv")
+n_repl <- get_prm(prm, "sample.tips.n.mc")
 
 
 # Deletions specifications:
 # NEW - 2021-04-27, copied from sung/test-for-sung.R
-prm.del <- list(alpha = 1,
+prm_del <- list(alpha = 1,
                 beta  = 6,
-                pos   = list(c(5:15),
-                             c(600:635)))
+                pos   = list(c(5:15, 200:250)))
 
 # Draw replicates from the probabilistic sequences
 # defined by the Beta-Uniform uncertainty model:
 seqs <- sung::draw_fasta_beta_unif(fastafile = fastafile,
                                    prm.beta  = btshp,
-                                   n.repl    = n.repl, 
-                                   prm.del   = NULL, #prm.del,
-                                   alphabet.type = 'nucleotide')
+                                   n.repl    = n_repl,
+                                   prm.del   = prm_del,
+                                   alphabet.type = "nucleotide")
 
 # Save the replicates to FASTA files:
-path.seqs <- paste0('seqs/seqs-prm-',args[1],'-mc')
+path_seqs <- paste0("seqs/seqs-prm-", args[1], "-mc")
 sung::export_fasta(seqs.replicates = seqs,
-                   fasta.name = path.seqs)
+                   fasta.name = path_seqs)
 
 
 # Save the total entropy
 # (summed over the sequence length):
-entr <- lapply(seqs, '[[', 3)
-sum.entropy <- sapply(entr, sum, na.rm=T)
-fname.out = 'entropy-prmset.out'
+entr <- lapply(seqs, "[[", 3)
+sum.entropy <- sapply(entr, sum, na.rm = T)
+fname_out <- "entropy-prmset.out"
 write.table(x = t(c(prmset = args[1],
                     s1 = btshp[1],
                     s2 = btshp[2],
                     mean(sum.entropy))),
-            file = fname.out,
-            append = TRUE, sep = ',',
+            file = fname_out,
+            append = TRUE, sep = ",",
             row.names = FALSE,
             col.names = FALSE, quote = FALSE)
