ed878097a66734b6778c45c3bcce5bd636087ee7
DBecker7 <d7becker@gmail.com>
2021-06-27 11:54:54 -0400

    Prep for downloading SAM files

--- RTT/sample-seqs_unif.R
+++ RTT/sample-seqs_unif.R
@@ -11,6 +11,9 @@ if("-N" %in% args){
 	samples_per_month <- as.numeric(args[which(args == "-N") + 1])
 }
 
+
+
+
 # Load and Filter Data --------------------------
 desc <- read.csv("sequences_descr_mt.csv")
 # Calculate number of lines per genome (each row is 60 nucleotides)
@@ -26,6 +29,8 @@ week_counts <- desc %>%
 desc <- left_join(desc, week_counts, by = c("yr", "mt")) %>% 
 	filter(n >= samples_per_month) %>%
 	filter(nchar(desc$SRAAccession) > 0)
+
+
 
 
 # Gather Information ----------------------------
@@ -53,14 +58,40 @@ if (FALSE) { # check that it's uniform
 }
 
 defs <- seq_per_week$def
-defs <- sapply(defs, function(x) strsplit(x, split = ":>")[[1]][2])
+defs <- sapply(defs, 
+	function(x) strsplit(x, split = ":>")[[1]][2]
+)
 
 writeLines(defs, con = "sampled_seqs.txt")
 
 
-# SRA Accession Numbers
 
+
+# SRA Accession Numbers -------------------------
+# Formatted for `seqtk subseq`
 sra <- seq_per_week$SRAAccession
 
 writeLines(sra, con = "sampled_SRA.txt")
+
+
+
+
+# Keep track of sampled SRA files ---------------
+# Assume that none have been downloaded yet
+new_sra <- data.frame(sra = sra, status = "Not Run")
+if (!"SRA_downloads.csv" %in% list.files()) {
+	write.csv(new_sra, file = "SRA_downloads.csv")
+} else {
+	# If it alread exists, append new accessions
+	# If it's already in the csv, don't add it.
+		# (retain the download status)
+	old_sra <- read.csv("SRA_downloads.csv")
+	new_sra <- new_sra[!new_sra$sra %in% old_sra$sra, ]
+	old_sra <- bind_rows(old_sra, new_sra)
+	write.csv(old_sra, "SRA_downloads.csv")
+}
+
+
+
+
 
