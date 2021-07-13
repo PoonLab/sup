e3dabb1a32f7c4b6459c08650639c6ca64be1914
DBecker7 <d7becker@gmail.com>
2021-07-13 10:50:10 -0400

    Generalized to accept other files

--- RTT/clean_names.R
+++ RTT/clean_names.R
@@ -1,6 +1,8 @@
-seq_names <- readLines("sampled_seqs_aligned_descr.txt")
+args <- commandArgs(TRUE)
+infile <- 
+seq_names <- readLines(args[1])
 # Remove the ">" at the start of every line
 seq_names <- substr(seq_names, 2, 1000)
 seq_dates <- sapply(strsplit(seq_names, split = "\\|"), function(x) x[3])
 write.csv(data.frame(name = seq_names, date = seq_dates), 
-	file = "sampled_metadata.csv", row.names = FALSE, quote = FALSE)
+	file = args[2], row.names = FALSE, quote = FALSE)
